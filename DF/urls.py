527039c982d43a270a8f2f88a503e5e4117ed08a
deodatojunior <deodatojunior999@gmail.com>
2021-11-25 19:01:34 -0400

    inclusão de tratamento de erro

--- DF/urls.py
+++ DF/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('erro/', views.erro, name='erro'),
     path('ativar_camera/', views.ativar_camera, name="ativar_camera")
 ]
--- DF/views.py
+++ DF/views.py
@@ -11,35 +11,43 @@ from pathlib import Path
 def index(request):
     return render(request, 'index.html')
 
+def erro(request):
+    return render(request, 'erro.html')
+
 def ativar_camera(request):
-    face_detector = cv2.CascadeClassifier(str(os.path.abspath('media/haarcascade_frontalface_default.xml')))
+    try:
 
-    video_capture = cv2.VideoCapture(0)
+        face_detector = cv2.CascadeClassifier(str(os.path.abspath('media/haarcascade_frontalface_default.xml')))
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = video_capture.read()
+        video_capture = cv2.VideoCapture(0)
 
-        image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        while True:
+            # Capture frame-by-frame
+            ret, frame = video_capture.read()
 
-        detections = face_detector.detectMultiScale(image_gray, minSize=(100,100), minNeighbors=5)
+            image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in detections:
-            print(w, h)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            detections = face_detector.detectMultiScale(image_gray, minSize=(100, 100), minNeighbors=5)
+            aspaoskapok
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in detections:
+                print(w, h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
 
-        cv2.putText(frame, 'Clique a letra Q para sair!', (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 0))
+            cv2.putText(frame, 'Clique a letra Q para sair!', (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1,
+                        (255, 255, 0))
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+        # When everything is done, release the capture
+        video_capture.release()
+        cv2.destroyAllWindows()
+        return redirect('index')
 
-    # When everything is done, release the capture
-    video_capture.release()
-    cv2.destroyAllWindows()
-    return redirect('index')
+    except:
 
+        return redirect('erro')
