63c6f291e62d97ef40ab954288acb3a0cfbd8666
deodatojunior <deodatojunior999@gmail.com>
2021-11-25 16:37:03 -0400

    Envio completo do sistema

---
+++ DF/views.py
@@ -0,0 +1,45 @@
+import ntpath
+import os.path
+
+from django.shortcuts import render, redirect
+import cv2
+from pathlib import Path
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def ativar_camera(request):
+    face_detector = cv2.CascadeClassifier(str(os.path.abspath('media/haarcascade_frontalface_default.xml')))
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Capture frame-by-frame
+        ok, frame = video_capture.read()
+
+        image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        detections = face_detector.detectMultiScale(image_gray)
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in detections:
+            print(w, h)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
+
+        cv2.putText(frame, 'Clique a letra Q para sair!', (10, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 0))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return redirect('index')
+
---
+++ README.md
@@ -0,0 +1,42 @@
+# Sistema de Detecção de Faces pela Webcam
+
+### Autor
+
+- Desenvolvedor: Deodato Silva
+- Email: deodatojunior999@gmail.com
+
+#### Frameworks
+
+- [Python 3.9.7](https://www.python.org/doc/)
+- [OpenCV](https://opencv.org)
+- [Django 3.2.9](https://www.djangoproject.com)
+- [Guia de estilo para código Python PEP8](https://www.python.org/dev/peps/pep-0008/)
+
+
+
+
+
+## Passos para executar a aplicação
+Primeiro, instale o Python no seu computador caso não tenha:
+
+[Python 3.9.7 para Windows](https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe)
+
+[Python 3.9.7 para Linux](https://www.python.org/ftp/python/3.10.0/Python-3.10.0.tgz)
+
+Dentro da pasta do repositório, digite isso:
+```
+venvDF/Scrips/activate.bat para WINDOWS
+
+source venvDF/bin/activate para Linux
+```
+Isso irá iniciar o ambiente virtual para executar o programa
+
+Após isso, digite este código para instalar os pacotes necessários dentro do ambiente virtual:
+```
+pip install -r requirements.txt
+```
+
+E após esse comando, execute o comando para iniciar a aplicação:
+```
+pythom manage.py runserver
+```
